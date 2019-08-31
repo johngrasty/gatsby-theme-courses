235c7c124bd2dbb7bf9eca05213ab9b25d04dcbb
John Grasty <johnpgrasty@gmail.com>
2019-08-31 10:13:37 +0200

    Added a custom 404 page.

--- demo/src/pages/404.js
+++ demo/src/pages/404.js
@@ -1,13 +1,30 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import React from 'react';
+import Layout from '../../../gatsby-theme-courses/src/components/Layout';
+
+const Wrapper = props => (
+  <div
+    {...props}
+    sx={{
+      px: 0,
+      py: '1rem',
+    }}
+  />
+);
 
 function NotFound() {
   return (
-    <h1>
-      Not found{' '}
-      <span role="img" aria-label="crying face">
-        😢
-      </span>
-    </h1>
+    <Layout>
+      <Wrapper>
+        <h1>
+          Not found{' '}
+          <span role="img" aria-label="crying face">
+            😢
+          </span>
+        </h1>
+      </Wrapper>
+    </Layout>
   );
 }
 
